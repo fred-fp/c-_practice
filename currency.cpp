83c05d5aee48a9f0d619d902bcf9a7cf65789fc5
fredfp <frederico.pedrozo@gmail.com>
2025-04-05 20:25:12 +0200

    feat: currency converter now keeps running until user chooses to quit, and keeps a running total allowing for the sum of many conversions.

--- currency.cpp
+++ currency.cpp
@@ -1,26 +1,73 @@
 #include <iostream>
-
+#include <iomanip>
+#include <string>
 using namespace std;
 
+// function to convert an amount of a currency to dollars
+double ConvertToUSD(double amount, int currency) {
+
+  // approximate currency conversion factors on 2025-04-05
+  switch(currency){
+    case 1: // USD
+      return amount;
+    case 2: // BRL
+      return amount * 0.1711;
+    case 3: // PEN
+      return amount * 0.27;
+    case 4: // COP
+      return amount * 0.00024;
+    case 5: // EUR
+      return amount * 1.1;
+    default:
+      cout << currency << " is not a supported number." << endl;
+      return 0;
+  }
+  return 0;
+}
+
+
 int main() {
-  // initialises variables for currencies
-  double col_pesos, bra_reais, per_soles, usd; 
+
+  // variables to track total
+  double total = 0, converted_amount = 0;
+
+  // variables to call conversion function
+  double amount = 0;
+  int currency;
+
+  // welcome message
+  cout << "Welcome to the currency conversion tool!" << endl;
+
+  // initialises loop
+  while(true) {
   
-  // initialises variables for conversions (approximate values on 2025-04-05)
-  double col_to_usd = 0.00024, bra_to_usd = 0.1711, per_to_usd = 0.27; 
+    // currency input
+    cout << "Choose currency:\n";
+    cout << "1: USD\n2: BRL\n3: PEN\n4: COP\n5: EUR\n0: quit" << endl;
+    cin >> currency;
+    
+    // quits if user chooses
+    if(currency == 0){
+      break; 
+    }
+    
+    // amount input
+    cout << "\nEnter amount: ";
+    cin >> amount;
+   
+    // applies conversion function, adds to total
+    converted_amount = ConvertToUSD(amount, currency);
+    total += converted_amount;
 
-  // asks for user input of each currency
-  cout << "Amount of Colombian pesos: ";
-  cin >> col_pesos;
-  cout << "Amount of Brazilian reais: ";
-  cin >> bra_reais;
-  cout << "Amount of Peruvian Soles: ";
-  cin >> per_soles;
+    // informs user of running total
+    cout << "\nAdded " << converted_amount << " USD." << endl;
+    cout << "Your current total is " << fixed << setprecision(2)
+    << total << " USD." << endl;
+    cout << "==============\n" << endl;
+  };
 
-  // applies conversions
-  usd = col_pesos * col_to_usd + bra_reais  * bra_to_usd + per_soles * per_to_usd;
-
-  cout << "Total in USD: $" << usd << endl;
+  // prints total to user
+  cout << "\nYour final total is " << total << " USD. Thanks for using the tool!" << endl;
 
   return 0;
 }
